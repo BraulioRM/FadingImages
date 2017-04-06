b839dd23ea5efd6cb5a2b48cb704e83d6f0e3e9b
BraulioRM <ramirez.braulioa@gmail.com>
2017-04-06 15:46:25 -0500

    bart image faded

--- app/src/main/java/braulio/ramirez/fadingimages/MainActivity.java
+++ app/src/main/java/braulio/ramirez/fadingimages/MainActivity.java
@@ -2,8 +2,19 @@ package braulio.ramirez.fadingimages;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void fade(View view){
+
+        ImageView bart = (ImageView)findViewById(R.id.bart);
+
+        bart.animate().alpha(0f).setDuration(2000);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
